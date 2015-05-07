9a4ecaa80dbd7000c88a3cd3745e6ecfceecc86b
Yurii Yarych <yura.yaruch@gmail.com>
2015-05-07 20:57:30 +0200

    Fix problems with uploader.

--- app/uploaders/lecture_cover_photo_uploader.rb
+++ app/uploaders/lecture_cover_photo_uploader.rb
@@ -16,13 +16,17 @@ class LectureCoverPhotoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+  def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.jpg"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
